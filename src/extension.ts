2caa0468cdfffbcd004b399190fd09977d6ea8a9
rumesh <rumeshk066@gmail.com>
2025-10-17 13:52:14 +0530

    feat: initialize CodeFlow AI extension with basic activation and component setup

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DataCollector } from './dataCollector';
+import { AIAnalyzer } from './aiAnalyzer';
+import { VisualizationPanel } from './visualization';
+import { GamificationSystem } from './gamification';
+import { BackendServices } from './backendServices';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codeflow-ai" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('codeflow-ai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeFlow AI!');
-	});
-
-	context.subscriptions.push(disposable);
+    console.log('CodeFlow AI is now active');
+    
+    // Initialize components
+    const dataCollector = new DataCollector(context);
+    const aiAnalyzer = new AIAnalyzer(context);
+    const visualizationPanel = new VisualizationPanel(context);
+    const gamificationSystem = new GamificationSystem(context);
+    const backendServices = new BackendServices(context);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    console.log('CodeFlow AI is now deactivated');
+}
