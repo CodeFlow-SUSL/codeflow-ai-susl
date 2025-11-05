1528b176f6ed1e8917722ab5fd2c4bb01451b033
Sewmini <sewminikumaranayaka13@gmail.com>
2025-11-05 11:59:17 +0530

    define ActivityType enum for different VS Code events

--- src/dataCollection/types.ts
+++ src/dataCollection/types.ts
@@ -4,3 +4,13 @@ export interface CodingActivity {
   type: ActivityType;  // Type of activity
   data: ActivityData;  // Activity-specific data
 }
+
+export enum ActivityType {
+  KEYSTROKE = 'keystroke',
+  SAVE = 'save',
+  SWITCH_FILE = 'switch_file',
+  OPEN_FILE = 'open_file',
+  CLOSE_FILE = 'close_file',
+  COMMAND = 'command'
+}
+
