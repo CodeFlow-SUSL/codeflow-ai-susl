2351a7b39ac9646f826acdccfe2bf080731d7843
Sewmini <sewminikumaranayaka13@gmail.com>
2025-11-12 18:53:06 +0530

    feat: add DataStorage class definition

--- src/dataCollection/dataStorage.ts
+++ src/dataCollection/dataStorage.ts
@@ -3,4 +3,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { DailyActivityLog, CodingActivity, ActivitySession, SessionMetadata } from './types';
 
+export class DataStorage {
+
+
 
