5b60ebcf02196f10a5a0e9735379c4fde30f1030
Sewmini <sewminikumaranayaka13@gmail.com>
2025-11-12 18:53:59 +0530

    feat: add context property to DataStorage class

--- src/dataCollection/dataStorage.ts
+++ src/dataCollection/dataStorage.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 import { DailyActivityLog, CodingActivity, ActivitySession, SessionMetadata } from './types';
 
 export class DataStorage {
+      private context: vscode.ExtensionContext;
+
 
 
 
