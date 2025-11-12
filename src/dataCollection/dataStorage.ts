a0093d11ecda648be2b5f3edac9bba6d73f7a47e
Sewmini <sewminikumaranayaka13@gmail.com>
2025-11-12 18:52:09 +0530

    refactor: organize imports in dataStorage module

--- src/dataCollection/dataStorage.ts
+++ src/dataCollection/dataStorage.ts
@@ -1,3 +1,6 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
+import { DailyActivityLog, CodingActivity, ActivitySession, SessionMetadata } from './types';
+
 
